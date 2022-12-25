80b7edeb6c4046bbb2e8e448b8668de930c4cef7
HylianFreddy <82058772+HylianFreddy@users.noreply.github.com>
2022-12-25 11:13:05 +0100

    Rename fairy functions

--- code/src/fairy.c
+++ code/src/fairy.c
@@ -8,7 +8,7 @@ void Fairy_StoreTargetActorType(u8 targetActorType) {
     lastTargetActorType = targetActorType;
 }
 
-void changeRainbowColorRGBAf(Color_RGBAf* color) {
+void Fairy_ChangeRainbowColorRGBAf(Color_RGBAf* color) {
 #define COLOR_SPEED 17 // 255 = 3*17*5
     if (color->r == 255.0f && color->g != 255.0f && color->b == 0)
         color->g += COLOR_SPEED;
@@ -24,27 +24,27 @@ void changeRainbowColorRGBAf(Color_RGBAf* color) {
         color->b -= COLOR_SPEED;
 }
 
-void updateNaviColors(EnElf* navi) {
+void Fairy_UpdateRainbowNaviColors(EnElf* navi) {
 
     if (lastTargetActorType == ACTORTYPE_PLAYER) {
         if (gSettingsContext.rainbowIdleNaviInnerColor)
-            changeRainbowColorRGBAf(&(navi->innerColor));
+            Fairy_ChangeRainbowColorRGBAf(&(navi->innerColor));
         if (gSettingsContext.rainbowIdleNaviOuterColor)
-            changeRainbowColorRGBAf(&(navi->outerColor));
+            Fairy_ChangeRainbowColorRGBAf(&(navi->outerColor));
     } else if (lastTargetActorType == ACTORTYPE_NPC) {
         if (gSettingsContext.rainbowNPCNaviInnerColor)
-            changeRainbowColorRGBAf(&(navi->innerColor));
+            Fairy_ChangeRainbowColorRGBAf(&(navi->innerColor));
         if (gSettingsContext.rainbowNPCNaviOuterColor)
-            changeRainbowColorRGBAf(&(navi->outerColor));
+            Fairy_ChangeRainbowColorRGBAf(&(navi->outerColor));
     } else if (lastTargetActorType == ACTORTYPE_ENEMY || lastTargetActorType == ACTORTYPE_BOSS) {
         if (gSettingsContext.rainbowEnemyNaviInnerColor)
-            changeRainbowColorRGBAf(&(navi->innerColor));
+            Fairy_ChangeRainbowColorRGBAf(&(navi->innerColor));
         if (gSettingsContext.rainbowEnemyNaviOuterColor)
-            changeRainbowColorRGBAf(&(navi->outerColor));
+            Fairy_ChangeRainbowColorRGBAf(&(navi->outerColor));
     } else {
         if (gSettingsContext.rainbowPropNaviInnerColor)
-            changeRainbowColorRGBAf(&(navi->innerColor));
+            Fairy_ChangeRainbowColorRGBAf(&(navi->innerColor));
         if (gSettingsContext.rainbowPropNaviOuterColor)
-            changeRainbowColorRGBAf(&(navi->outerColor));
+            Fairy_ChangeRainbowColorRGBAf(&(navi->outerColor));
     }
 }
