19e417240f85a8672fa5a19fcb4ad901fdbb0343
HylianFreddy <82058772+HylianFreddy@users.noreply.github.com>
2023-07-15 15:07:06 +0200

    Skulltula Hints (missing translations)

---
+++ code/src/actors/skulltula_people.c
@@ -0,0 +1,10 @@
+#include "settings.h"
+
+#define EnSsh_Update ((ActorFunc)0x1DF0A4)
+
+void EnSsh_rUpdate(Actor* thisx, GlobalContext* globalCtx) {
+    EnSsh_Update(thisx, globalCtx);
+    if (gSettingsContext.skulltulaHints && thisx->textId == 0x22) {
+        thisx->textId = 0x9400 + thisx->params - 1;
+    }
+}
