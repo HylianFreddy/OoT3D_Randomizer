32706758c71c055defa90867d27b6423a97bb4b5
HylianFreddy <82058772+HylianFreddy@users.noreply.github.com>
2025-03-10 16:43:28 +0100

    merge skulltula types

--- source/enemizer.hpp
+++ source/enemizer.hpp
@@ -15,7 +15,8 @@ enum class LocationType {
     ABOVE_VOID,   // Location is over a void plane. Enemy must be able to fly.
     UNDERWATER,   // Location is underwater. Enemy must be defeatable with hookshot and iron boots.
     ABOVE_WATER,  // Location is in the air above a water surface. Enemy must be able to float or fly.
-    SPAWNER,      // Location is a grounded enemy spawner (stalchildren, leevers)
+    SPAWNER,      // Location is a grounded enemy spawner (stalchildren, leevers).
+    NEVER,        // Do not assign to any location.
 };
 
 // Enemy requirements to pass to the patch to edit the ActorEntry at runtime.
@@ -23,6 +24,70 @@ enum class EnemyRequirement {
     ON_GROUND,        // Ground-based enemy that doesn't work in mid-air, it should be spawned at ground level.
     AT_WATER_SURFACE, // Water-based enemy that doesn't work in mid-air, it should be spawned at water surface.
     IN_MID_AIR,       // Flying enemy that doesn't work at ground level, it should be spawned up in the air.
+};
+
+enum class EnemyId {
+  ENEMY_POE,
+  ENEMY_STALFOS,
+  ENEMY_OCTOROK,
+  ENEMY_WALLMASTER,
+  ENEMY_DODONGO,
+  ENEMY_KEESE_NORMAL,
+  ENEMY_KEESE_FIRE,
+  ENEMY_KEESE_ICE,
+  ENEMY_TEKTITE_RED,
+  ENEMY_TEKTITE_BLUE,
+  ENEMY_LEEVER,
+  ENEMY_PEAHAT,
+  ENEMY_PEAHAT_LARVA,
+  ENEMY_LIZALFOS,
+  ENEMY_DINOLFOS,
+  ENEMY_GOHMA_LARVA,
+  ENEMY_SHABOM,
+  ENEMY_DODONGO_BABY,
+  ENEMY_DARK_LINK,
+  ENEMY_BIRI,
+  ENEMY_TAILPASARAN,
+  ENEMY_SKULLTULA,
+  ENEMY_TORCH_SLUG,
+  ENEMY_STINGER_FLOOR,
+  ENEMY_MOBLIN_CLUB,
+  ENEMY_MOBLIN_SPEAR,
+  ENEMY_ARMOS,
+  ENEMY_DEKU_BABA_SMALL,
+  ENEMY_DEKU_BABA_BIG,
+  ENEMY_MAD_SCRUB,
+  ENEMY_BARI,
+  ENEMY_BUBBLE_BLUE,
+  ENEMY_BUBBLE_FIRE,
+  ENEMY_BUBBLE_GREEN,
+  ENEMY_FLYING_FLOOR_TILE,
+  ENEMY_BEAMOS,
+  ENEMY_FLOORMASTER,
+  ENEMY_REDEAD,
+  ENEMY_GIBDO,
+  ENEMY_POE_SISTER,
+  ENEMY_DEAD_HAND_HAND,
+  ENEMY_SKULLWALLTULA,
+  ENEMY_FLARE_DANCER,
+  ENEMY_DEAD_HAND,
+  ENEMY_SHELL_BLADE,
+  ENEMY_BIG_OCTO,
+  ENEMY_WITHERED_DEKU_BABA,
+  ENEMY_LIKE_LIKE,
+  ENEMY_PARASITIC_TENTACLE,
+  ENEMY_SPIKE,
+  ENEMY_ANUBIS,
+  ENEMY_IRON_KNUCKLE,
+  ENEMY_SKULL_KID,
+  ENEMY_FLYING_POT,
+  ENEMY_FREEZARD,
+  ENEMY_STINGER_WATER,
+  ENEMY_GERUDO_FIGHTER,
+  ENEMY_WOLFOS,
+  ENEMY_STALCHILD,
+  ENEMY_GUAY,
+  ENEMY_MAX,
 };
 
 class EnemyType {
