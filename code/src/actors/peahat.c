cd5ecc5770bbedece27287eb644c7f4527872315
HylianFreddy <82058772+HylianFreddy@users.noreply.github.com>
2025-03-15 16:45:23 +0100

    Peahat: follow player height

--- code/src/actors/peahat.c
+++ code/src/actors/peahat.c
@@ -3,7 +3,11 @@
 
 #define EnPeehat_Update ((ActorFunc)GAME_ADDR(0x167644))
 
+#define STATE_SEEK_PLAYER 14
+
 void EnPeehat_rUpdate(Actor* thisx, GlobalContext* globalCtx) {
+    EnPeehat* this = (EnPeehat*)thisx;
+
     EnPeehat_Update(thisx, globalCtx);
 
     if (gSettingsContext.enemizer == ON && thisx->params == (s16)0xFFFF) { // Grounded Peahat
@@ -12,9 +16,14 @@ void EnPeehat_rUpdate(Actor* thisx, GlobalContext* globalCtx) {
         s32 waterBoxFound = WaterBox_GetSurfaceImpl(gGlobalContext, &gGlobalContext->colCtx, thisx->world.pos.x,
                                                     thisx->world.pos.z, &yWaterSurface, &waterBox);
 
-        // Make Peahats float on water so they can work as aquatic plants.
+        // Float on water to work as an aquatic plant.
         if (waterBoxFound && thisx->world.pos.y < yWaterSurface) {
             thisx->world.pos.y = yWaterSurface;
+        }
+
+        // Fly at player's height.
+        if (this->state == STATE_SEEK_PLAYER && thisx->world.pos.y < PLAYER->actor.world.pos.y) {
+            Math_SmoothStepToF(&thisx->world.pos.y, PLAYER->actor.world.pos.y, 3, 5.0, 0);
         }
     }
 }
