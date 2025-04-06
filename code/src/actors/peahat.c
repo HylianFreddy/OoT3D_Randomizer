145d064429f0690e258478a7cc14d85194802225
HylianFreddy <82058772+HylianFreddy@users.noreply.github.com>
2025-04-06 17:31:24 +0200

    Soulless Peahat Larva prevent death

--- code/src/actors/peahat.c
+++ code/src/actors/peahat.c
@@ -1,7 +1,11 @@
 #include "peahat.h"
 #include "settings.h"
+#include "enemy_souls.h"
 
 #define EnPeehat_Update ((ActorFunc)GAME_ADDR(0x167644))
+
+#define EnPeehat_StateAttackRecoil ((EnPeehatActionFunc)GAME_ADDR(0x2B2CC0))
+#define EnPeehat_Larva_StateSeekPlayer ((EnPeehatActionFunc)GAME_ADDR(0x391588))
 
 #define STATE_SEEK_PLAYER 14
 
@@ -29,5 +33,22 @@ void EnPeehat_rUpdate(Actor* thisx, GlobalContext* globalCtx) {
         // Vanilla code doesn't check for ceilings.
         Actor_UpdateBgCheckInfo(globalCtx, thisx, 25.0f, 30.0f, 100.0f,
                                 UPDBGCHECKINFO_WALL | UPDBGCHECKINFO_CEILING | UPDBGCHECKINFO_FLOOR_WATER);
+    }
+
+    if (!EnemySouls_CheckSoulForActor(thisx) && thisx->params == 1) { // Peahat Larva
+        // Prevent death when hitting player's shield.
+        if (this->actionFunc == EnPeehat_StateAttackRecoil) {
+            this->actionFunc = EnPeehat_Larva_StateSeekPlayer;
+            this->state      = STATE_SEEK_PLAYER;
+            this->colliderQuadBase.atFlags &= ~(1 << 2); // AT_BOUNCED
+        }
+
+        // Prevent death when hitting ground.
+        CollisionPoly floorPoly;
+        f32 yGroundIntersect = BgCheck_RaycastDown1(&gGlobalContext->colCtx, &floorPoly, &thisx->world.pos);
+
+        if (thisx->world.pos.y < yGroundIntersect + 10) {
+            thisx->world.pos.y = yGroundIntersect + 10;
+        }
     }
 }
