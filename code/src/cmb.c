cb533c5dbcd282c8987d76af5282cb09d3d3e4f7
HylianFreddy <82058772+HylianFreddy@users.noreply.github.com>
2026-04-14 21:54:28 +0200

    working tektite switching

--- code/src/cmb.c
+++ code/src/cmb.c
@@ -8,23 +8,25 @@
 
 #define GET_STRUCT_FIELD(structPointer, type, offset) (*(type*)((u32)structPointer + offset))
 
+static u8 black = 1;
+
 void CmbManager_BeforeInit(CmbManager* cmbMan) {
-    // if (!gIsForSoullessActor || gSettingsContext.soullessEnemiesLook != SOULLESSLOOK_BLACK) {
-    //     return;
-    // }
-    if (!IsInGame()) {
+    if (!gIsForSoullessActor || gSettingsContext.soullessEnemiesLook != SOULLESSLOOK_BLACK) {
         return;
     }
-    return;
 
     CMB_MATS* cmbMats = Cmb_GetMatsChunk(cmbMan->cmbChunk);
     for (size_t i = 0; i < cmbMats->materialCount; i++) {
         Material* mat = &cmbMats->materials[i];
 
-        if (gIsForSoullessActor) {
+        if (black) {
             mat->textureMappersUsed = 0;
+            mat->alphaTestEnabled = 0x0;
+            mat->blendMode        = 0x0;
         } else if (mat->textureMappersUsed == 0) {
             mat->textureMappersUsed = 1;
+            mat->alphaTestEnabled = 0x1;
+            mat->blendMode        = 0x1;
         }
 
         // mat->textureMappersUsed = 0; // good for black
@@ -55,9 +57,10 @@ typedef struct EnTite {
 #define SkelAnime_Init                                                                                       \
     ((void (*)(Actor * actor, GlobalContext * globalCtx, SkelAnime * skelAnime, s32 cmbIndex, s32 csabIndex, \
                void* jointTable, void* morphTable, s32 limbCount))0x353c9c)
-#define Animation_ChangeImpl                                                                                     \
-    ((void (*)(SkelAnime * anime, s32 animation_index, f32 play_speed, f32 start_frame, f32 end_frame, s32 mode, \
-               f32 morph_frames, s32 taper))0x35302c)
+typedef void (*Foo)(SkelAnime* anime, s32 animation_index, f32 play_speed, f32 start_frame, f32 end_frame, s32 mode,
+                    f32 morph_frames, s32 taper) __attribute__((pcs("aapcs-vfp")));
+#define Animation_ChangeImpl ((Foo)0x35302c)
+#define Animation_PlayLoop ((void (*)(SkelAnime * anime, s32 animation_index))0x36e734)
 
 void CmbManager_ReInitTektite(void) {
     ObjectEntry* obj = Object_FindEntryOrSpawn(0x16);
@@ -66,9 +69,8 @@ void CmbManager_ReInitTektite(void) {
     ZAR_Destroy(zarInfo);
     ZAR_SetupZARInfo(zarInfo, obj->buf, obj->size, 0);
     // Re-initialize CMBManager
-    static u8 black = 1;
     black ^= 1;
-    gIsForSoullessActor = black;
+    gIsForSoullessActor = 1;
     ZAR_GetCMBByIndex(zarInfo, 0);
     gIsForSoullessActor = 0;
 
@@ -93,19 +95,22 @@ void CmbManager_ReInitTektite(void) {
             // EnTite_Init(((EnTite*)actor), gGlobalContext);
 
             SkelAnime* anime = &((EnTite*)actor)->skelAnime;
-            s32 animIndex    = anime->animIndex;
-            // f32 curFrame     = anime->curFrame;
-            // f32 playSpeed    = anime->playSpeed;
-            // f32 startFrame   = anime->startFrame;
-            // f32 endFrame     = anime->endFrame;
-            // f32 animMode     = anime->animMode;
+            s32 animIndex    = anime->animIndex; (void)animIndex;
+            f32 curFrame     = anime->curFrame; (void)curFrame;
+            f32 playSpeed    = anime->playSpeed; (void)playSpeed;
+            f32 startFrame   = anime->startFrame; (void)startFrame;
+            f32 endFrame     = anime->endFrame; (void)endFrame;
+            f32 animMode     = anime->animMode; (void)animMode;
+
             SkelAnime_Destroy(anime);
+            SkelAnime_Init(actor, gGlobalContext, anime, 0, 0, actor + 0x228,
+                           actor + 0x430, 10);
+            // Animation_ChangeImpl(anime, 0, 1.0, 1.0, 1.0, 0, 4.0, 0);
             // EnTite_Init(((EnTite*)actor), gGlobalContext);
-            // SkelAnime_Init(actor, gGlobalContext, anime, 0, 0, &GET_STRUCT_FIELD(actor, void*, 0x228),
-            //                &GET_STRUCT_FIELD(actor, void*, 0x430), 10);
-            // Animation_ChangeImpl(anime, animIndex, playSpeed, startFrame, endFrame, animMode, 0.0, 0);
-            Animation_ChangeImpl(anime, animIndex, 1.0, 1.0, 1.0, 0, 4.0, 0);
-            GET_STRUCT_FIELD(actor, u32, 0x63c) = 0x284724;
+
+            // Animation_PlayLoop(anime, 1);
+            Animation_ChangeImpl(anime, animIndex, playSpeed, startFrame, endFrame, animMode, 0.0, 0);
+            // GET_STRUCT_FIELD(actor, u32, 0x63c) = 0x284724;
             // anime->curFrame = curFrame;
 
             // CitraPrint("((EnTite*)actor)->skelAnime.cmbMan = %X", ((EnTite*)actor)->skelAnime.cmbMan);
