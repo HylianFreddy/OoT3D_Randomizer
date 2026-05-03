ed174ae85c722e0ce3e845e28686531ea5f5ba8d
HylianFreddy <82058772+HylianFreddy@users.noreply.github.com>
2026-05-03 17:35:26 +0200

    Remove bosses

--- code/src/actors/ganondorf.c
+++ code/src/actors/ganondorf.c
@@ -1,10 +1,5 @@
 #include "z3D/z3D.h"
 #include "ganondorf.h"
-#include "actor.h"
-
-/*-------------------------------
-|           BossGanon           |
--------------------------------*/
 
 void BossGanon_Update(Actor* thisx, GlobalContext* globalCtx);
 
@@ -17,20 +12,4 @@ void BossGanon_rUpdate(Actor* thisx, GlobalContext* globalCtx) {
         // Don't fill health if player has 0 hearts.
         gSaveContext.healthAccumulator = prevHealthAccumulator;
     }
-}
-
-/*-------------------------------
-|         BossGanondrof         |
--------------------------------*/
-
-void BossGanondrof_ReinitModels(BossGanondrof* this) {
-    Actor_ReinitSkelAnime(&this->actor, &this->anime, 1);
-}
-
-/*-------------------------------
-|             EnFHG             |
--------------------------------*/
-
-void EnFHG_ReinitModels(EnFHG* this) {
-    Actor_ReinitSkelAnime(&this->actor, &this->anime, 0);
 }
