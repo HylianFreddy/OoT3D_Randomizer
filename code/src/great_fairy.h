0b0429553ee118fa944e1ac611e4b286ec5d7eb1
HylianFreddy <82058772+HylianFreddy@users.noreply.github.com>
2026-08-12 22:07:41 +0200

    Move Great Fairy stuff

---
+++ code/src/great_fairy.h
@@ -0,0 +1,5 @@
+#include "z3D/z3D.h"
+#include "z3D/actors/z_bg_dy_yoseizo.h"
+
+void BgDyYoseizo_rInit(Actor* thisx, GlobalContext* globalCtx);
+void BgDyYoseizo_rUpdate(Actor* thisx, GlobalContext* globalCtx);
